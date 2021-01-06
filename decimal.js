c99f62049cd5405ab0e53aaac7986f276737c374
hungle <hungle@hungles-iMac.local>
2021-01-06 23:35:03 +0700

    fix decimal

--- decimal.js
+++ decimal.js
@@ -1,6 +1,12 @@
 
 decimal(number,length){
-  	var s = number.toString();
-	if (s.indexOf('.') == -1) s += '.';
-	while (s.length < s.indexOf('.') + length) s += '0';
+  	let s = number.toString();
+    if(s.indexOf(".") == -1) s += ".";
+    let index = s.indexOf(".") + length;
+    if(s.length >= index){
+        s = s.slice(0,index);
+    }else{
+        while(s.length < index) s += "0";
+    }
+    return s;
 }
